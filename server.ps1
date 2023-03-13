5ecc298bd18a79abece445b29aa7ae976c1f0b9e
Circl3s <maak4422@gmail.com>
2023-03-13 16:41:52 +0100

    Initial commit

---
+++ server.ps1
@@ -0,0 +1,24 @@
+Import-Module Pode
+
+Start-PodeServer {
+    Add-PodeEndpoint -Address localhost -Port 1234 -Protocol Http
+
+    Set-PodeViewEngine -Type Pode
+
+    Add-PodeRoute -Method GET -Path "/" -ScriptBlock {
+        Write-PodeViewResponse -Path "index"
+    }
+
+    Add-PodeRoute -Method GET -Path "/info" -ScriptBlock {
+        $Counters = Get-Counter '\Processor(_Total)\% Processor Time', '\Memory\Available MBytes'
+        $CPU = $Counters.CounterSamples[0].CookedValue
+        $RAM = $Counters.CounterSamples[1].CookedValue
+        $Processes = (Get-Process).Length
+        Write-PodeJsonResponse -Value @{"processes" = $Processes; "cpu" = $CPU; "ram" = $RAM}
+    }
+
+    Add-PodeRoute -Method POST,GET -Path "/launch/:path" -ScriptBlock {
+        Start-Process $WebEvent.Parameters["path"]
+        Write-PodeJsonResponse -Value @{"success" = $?}
+    }
+}
